aeb4d64078cb3c1d5a9c57564acffa6f760c9961
gao-mu <3530553117@qq.com>
2023-02-25 11:43:27 +0800

    feat: 添加记录堆栈错误信息工具

---
+++ errors/stack_test.go
@@ -0,0 +1,64 @@
+// Package errors
+// author gmfan
+// date 2023/2/24
+package errors
+
+import (
+	"encoding/json"
+	stderrors "errors"
+	"testing"
+)
+
+func TestIsStackError(t *testing.T) {
+	tests := []struct {
+		name string
+		err  error
+		want bool
+	}{
+		{
+			name: "Nil test",
+			err:  nil,
+			want: false,
+		},
+		{
+			name: "普通 error",
+			err:  stderrors.New("test"),
+			want: false,
+		},
+		{
+			name: "stackError",
+			err:  New("stackError"),
+			want: true,
+		},
+	}
+
+	for _, tt := range tests {
+		_, got := tt.err.(*stackError)
+		if got != tt.want {
+			t.Errorf("IsStackError(): name: %s, got: %v, want: %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func TestStackError_Json(t *testing.T) {
+	tests := []error{
+		New("new"),
+		Wrap(New("warp")),
+		Wrapf(New("wrapf"), "format %d", 1),
+	}
+
+	for _, tt := range tests {
+		got := tt.(Jsoner).Json()
+		if !json.Valid([]byte(got)) {
+			t.Errorf("stackError.Json(): 输出字符串不是 JSON 格式, got: %s", got)
+		}
+	}
+}
+
+func BenchmarkStackError_Json(b *testing.B) {
+	err := Wrapf(New("json test"), "wrap message")
+	st, _ := err.(*stackError)
+	for i := 0; i < b.N; i++ {
+		st.Json()
+	}
+}
